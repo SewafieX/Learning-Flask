7f1ec985ea7f3ba5f650045369c46c423061bfce
Ahmed Sewify <ahmed.sewify@connect.qut.edu.au>
2018-06-05 12:08:44 +1000

    About page

--- routes.py
+++ routes.py
@@ -4,12 +4,20 @@ and return the page index.html"""
 from flask import Flask, render_template 
 #creating a new usable instance of the Flask class
 app = Flask(__name__)
-#map the url "/" int the function index
+
+#map the url "/" into the function index
 @app.route("/")
 #This python function uses the flask function render_template 
 #to render "index.html"
 def index():
 	return render_template("index.html")
+
+@app.route("/about")
+#This python function uses the flask function render_template 
+#to render "about.html"
+def about():
+	return render_template("about.html")
+
 
 if __name__ == "__main__":
 	#app.run runs the app on a local server
